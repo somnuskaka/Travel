8cec20f51a94f34e93322cc4172b60437d846de0
bigbear <somnuskaka@163.com>
2020-01-02 10:02:08 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,11 +2,12 @@
   <div>
     <city-header>国际/国内</city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from "./components/Header";
 import CitySearch from "./components/Search";
 import CityList from "./components/List";
@@ -18,6 +19,30 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data (){
+    return {
+      cities:{},
+      hotCities:[]
+    }
+  },
+  methods:{
+    getCityInfo (){
+      axios.get('/api/city.json')
+       .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res){
+      res=res.data
+      if(res.ret && res.data){
+        const data = res.data
+        this.cities =data.cities
+        this.hotCities=data.hotCities
+      }
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 };
 </script>
